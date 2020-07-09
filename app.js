fcef0679ddcf9d0709dd040fdb2886441aa3559f
Grigorii Dvoeglazov <dvoegor@gmail.com>
2020-07-09 22:39:31 +0400

    login and register templates

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 const session = require('express-session');
 
 var indexRouter = require('./routes/index');
+var loginRouter = require('./routes/login');
+var registerRouter = require('./routes/register');
 
 var app = express();
 
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({secret: 'big', saveUninitialized: false, resave: false}));
 
 app.use('/', indexRouter);
+app.use('/login', loginRouter);
+app.use('/register', registerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/register.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+const pool = require('../pool/pool');
+
+/* GET home page. */
+router.get('/', async (req, res) => {
+    const promisePool = pool.promise();
+    const persons = [rows] = await promisePool.query("SELECT id FROM personnel");
+    const profiles = [rows] = await promisePool.query("SELECT id FROM profiles");
+    console.log([persons[0], profiles[0]])
+    res.render('register', { persons: persons[0], profiles: profiles[0] })
+})
+
+module.exports = router;
